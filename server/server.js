b7b4c92c8d8dabc3fb7822a732ddd8c122783b89
Ishmael Destiny Onwe <theimpactindex@gmail.com>
2025-04-04 20:05:53 +0100

    Update server/server.js

--- server/server.js
+++ server/server.js
@@ -1,21 +1,20 @@
-require('dotenv').config();
 const express = require('express');
-const db = require('./config/db');
-const userRoutes = require('./User/routes');
+const mongoose = require('mongoose');
+const assetRoutes = require('./assetManagement/routes');
 
 const app = express();
-
 app.use(express.json());
 
-db.then(() => {
-  console.log('Connected to MongoDB');
-  app.use('/api/users', userRoutes);
+const PORT = process.env.PORT || 5000;
 
-  app.use((err, req, res, next) => {
-    res.status(500).json({ error: err.message });
-  });
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error(err));
 
-  app.listen(process.env.PORT, () => {
-    console.log(`Server running on port ${process.env.PORT}`);
-  });
-}).catch(err => console.error(err));
+// Use asset management routes
+app.use('/api', assetRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
