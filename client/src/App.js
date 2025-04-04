4a1935417e658d7db88c45f81b09cba7864d658f
Ishmael Destiny Onwe <theimpactindex@gmail.com>
2025-04-04 20:06:36 +0100

    Update client/src/App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import AssetManagement from './pages/AssetManagement';
+import Navbar from './components/Navbar';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ContactPage from './pages/ContactPage';
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <Switch>
-        <Route path="/assets" component={AssetManagement} />
-        <Route path="/" exact component={() => <div>Home Page</div>} />
+        <Route path="/" exact component={HomePage} />
+        <Route path="/about" component={AboutPage} />
+        <Route path="/contact" component={ContactPage} />
       </Switch>
     </Router>
   );
