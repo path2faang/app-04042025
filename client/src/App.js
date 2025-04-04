289e845d7c860e50277db986481353569853f4bc
Ishmael Destiny Onwe <theimpactindex@gmail.com>
2025-04-04 20:06:48 +0100

    Update client/src/App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import HomePage from './pages/HomePage';
-import AboutPage from './pages/AboutPage';
-import ContactPage from './pages/ContactPage';
+import HelpSupportPage from './pages/HelpSupportPage';
 
 function App() {
   return (
     <Router>
-      <Navbar />
       <Switch>
-        <Route path="/" exact component={HomePage} />
-        <Route path="/about" component={AboutPage} />
-        <Route path="/contact" component={ContactPage} />
+        <Route path="/help-support" component={HelpSupportPage} />
       </Switch>
     </Router>
   );
