4e4ffcd6d86fbc4bbc9dfc71ec3700170504f8b9
Ishmael Destiny Onwe <theimpactindex@gmail.com>
2025-04-04 20:06:44 +0100

    Update server/server.js

--- server/server.js
+++ server/server.js
@@ -1,18 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const communicationRoutes = require('./communication/routes');
 require('dotenv').config();
-
+const helpSupportRoutes = require('./helpSupport/routes');
 const app = express();
 
 app.use(express.json());
-app.use('/api/communication', communicationRoutes);
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+app.use('/api/help-support', helpSupportRoutes);
 
 const PORT = process.env.PORT || 5000;
-const DB_URI = process.env.MONGODB_URI;
-
-mongoose.connect(DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => {
-        app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-    })
-    .catch(error => console.error(error));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
